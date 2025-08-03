3a459dde9d0e7224343457ed087cdea36af15402
JupiterPi <hello@jupiterpi.de>
2025-08-04 00:29:27 +0200

    feat(ui): light theme app icon

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,11 @@
 import { UserButton } from "@clerk/clerk-react"
 import { DarkModeToggle } from "./components/dark-mode-toggle"
+import { useTheme } from "./components/theme-provider"
 
 export default function App({ children }: { children: React.ReactNode }) {
   return <>
     {/* navbar */}
-    <div className="border-b-1 border-black px-4 py-3 flex gap-3 items-center">
+    <div className="border-b shadow-sm px-4 py-3 flex gap-3 items-center">
         <TitleWithLogo />
         <div className="flex-1"></div>
         <DarkModeToggle />
@@ -16,9 +17,11 @@ export default function App({ children }: { children: React.ReactNode }) {
 }
 
 function TitleWithLogo() {
+  const isDarkTheme = useTheme().theme === "dark"
+
   return (
     <h1 className="font-semibold text-2xl flex items-center gap-2">
-      <img src="/andpaper-icon-dark.png" className="h-[25px] relative -top-[0px]" />
+      <img src={isDarkTheme ? "/andpaper-icon.png" : "/andpaper-icon-dark.png"} className="h-[25px] relative -top-[0px]" />
       <span className="select-none">AndPaper</span>
     </h1>
   )
