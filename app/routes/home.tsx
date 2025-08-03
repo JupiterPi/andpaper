ac39a5e9958941722a419965ada0361363414b64
JupiterPi <hello@jupiterpi.de>
2025-08-03 16:51:46 +0200

    remove template clutter

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,18 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Home" },
+    { name: "description", content: "Welcome to andpaper!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="flex flex-col justify-center items-center h-screen">
+    <h1 className="font-semibold text-xl">Welcome to andpaper!</h1>
+
+    <div className="mt-4">
+      <p className="text-gray-600">This is a simple example of a home page layout.</p>
+    </div>
+  </main>;
 }
